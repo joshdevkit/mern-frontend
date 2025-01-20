cb7d3adfb4ad7d28e18c5f1ccb7d225730811ba3
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2025-01-20 10:39:27 +0800

    final commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
+import { API_URL } from "@/server";
 import axios from "axios";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { API_URL } from "@/server";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
